3e6ea74aafec71affb5da516c5772240496e3f69
Aurel Avramescu <aurel.avramescu@gmail.com>
2017-11-28 00:34:31 +0100

    Small changes for trying to move forward

--- app/__init__.py
+++ app/__init__.py
@@ -5,4 +5,5 @@ from flask import Flask
 from .views import init_views
 
 app = Flask(__name__)
+
 init_views(app)
--- app/views.py
+++ app/views.py
@@ -2,49 +2,85 @@ import app.engine as engine
 import app.direction as direction
 import app.video_dir as video_dir
 from flask import request
+from flask import json
 
 def init_views(app):
     @app.route('/')
     def init_robot():
         engine.setup()
         direction.setup()
-        return 'Robot has been initialized!'
+        response = app.response_class(
+            response=json.dumps({'message': 'Robot has been initialized!'}),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
 
 
     @app.route('/car/forward')
     def move_forward():
         engine.forwardWithSpeed(50)
-        return 'Move forward'
+        response = app.response_class(
+            response=json.dumps({'message': 'Move forward'}),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
 
 
     @app.route('/car/backward')
     def move_backward():
         engine.backwardWithSpeed(50)
-        return 'Move forward'
+        response = app.response_class(
+            response=json.dumps({'message': 'Move backward'}),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
 
 
     @app.route('/car/left')
     def turn_left():
         direction.turn_left()
-        return 'Turn left'
+        response = app.response_class(
+            response=json.dumps({'message': 'Turn left'}),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
 
 
     @app.route('/car/right')
     def turn_right():
         direction.turn_right()
-        return 'Turn right'
+        response = app.response_class(
+            response=json.dumps({'message': 'Turn ritght'}),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
 
 
     @app.route('/car/home')
     def home():
         direction.home()
-        return 'Home'
+        response = app.response_class(
+            response=json.dumps({'message': 'Home'}),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
 
 
     @app.route('/car/stop')
     def stop():
         engine.stop()
-        return 'Stop'
+        response = app.response_class(
+            response=json.dumps({'message': 'Stop engine'}),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
 
 
     @app.route('/camera/dir/calibrate')
@@ -52,24 +88,51 @@ def init_views(app):
         x = int(request.args.get("x"))
         y = int(request.args.get("y"))
         video_dir.calibrate(x, y)
-        return 'Calibrate'
+        response = app.response_class(
+            response=json.dumps({'message': 'Calibrate'}),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
+
 
     @app.route('/camera/up')
     def move_up():
         video_dir.move_increase_y()
-        return "Up"
+        response = app.response_class(
+            response=json.dumps({'message': 'Camera up'}),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
+
 
     @app.route('/camera/down')
     def move_down():
         video_dir.move_decrease_y()
-        return "Down"
+        response = app.response_class(
+            response=json.dumps({'message': 'Camera down'}),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
 
     @app.route('/camera/left')
     def move_left():
         video_dir.move_decrease_x()
-        return "Left"
+        response = app.response_class(
+            response=json.dumps({'message': 'Camera left'}),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
 
     @app.route('/camera/right')
     def move_right():
         video_dir.move_increase_x()
-        return "Right"
+        response = app.response_class(
+            response=json.dumps({'message': 'Camera right'}),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
